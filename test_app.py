fa3fd997bb7613d15100752fc04d0d926b09073c
Richard Deodutt <richarddeodutt@gmail.com>
2022-10-10 20:36:56 -0400

    Update

--- test_app.py
+++ test_app.py
@@ -3,8 +3,8 @@ from application import app, greet
 def test_quick():
   a = "jeff"
   greeting = greet(a)
-  assert greeting == "Hi jeff "
+  assert greeting == "Hi jeff"
 
-# def test_home_page():
-#     response = app.test_client().get('/')
-#     assert response.status_code == 200
+ def test_home_page():
+     response = app.test_client().get('/')
+     assert response.status_code == 200
